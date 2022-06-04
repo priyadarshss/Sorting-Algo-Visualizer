b3cad83aa1d60a1367ac00740e8e52cf53527abf
priyadarshss <priyadarsh2001@gmail.com>
2022-06-04 23:51:48 +0530

    added bars

--- main.py
+++ main.py
@@ -12,12 +12,18 @@ class DrawInformation:
 	SIDE_PAD = 100
 	TOP_PAD = 150
 
+	GRADIENTS = [
+		(128, 128, 128),
+		(160, 160, 160),
+		(192, 192, 192)
+	]
+
 	def __init__(self, width, height, lst):
 		self.width = width
 		self.height = height
 
 		self.window = pygame.display.set_mode((width, height))
-		pygame.display.set_caption("Sorting Algorithm Visualization")
+		pygame.display.set_caption("Sorting Algorithm Visualizer")
 		self.set_list(lst)
 
 	def set_list(self, lst):
@@ -38,13 +44,35 @@ def generate_starting_list(n, min_val, max_val):
 
 	return lst
 
+def draw(draw_info):
+	draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+	pygame.display.update()
+
+
+def draw_list(draw_info):
+	lst = draw_info.lst
+
+	for i, val in enumerate(lst):
+		x = draw_info.start_x + i * draw_info.block_width
+		y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+	
+
 def main():
 	run = True
 	clock = pygame.time.Clock()
+	
+	n = 50
+	min_val = 0
+	max_val = 100
+
+	lst = generate_starting_list(n, min_val, max_val)
+	draw_info = DrawInformation(800, 600, lst)
 
 	while run:
 		clock.tick(60)
 		pygame.display.update()
+
+		draw(draw_info)
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
